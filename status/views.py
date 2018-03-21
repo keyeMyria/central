1918bdaa0343ab52e2271d26be63feb77e36706c
Jean Ribes <jean.christophe.ribes@sfr.fr>
2018-03-21 13:53:26 +0100

    test permissions

--- status/views.py
+++ status/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-
+from rest_framework import permissions
 from status.serializers import *
 from status.models import *
 
 # Create your views here.
 
 class HostsOverview(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = HostSerializer
     queryset = HostStatus.objects.all()
 
 class InfosViewSet(viewsets.ModelViewSet):
+
     serializer_class = InfoSerializer
     def get_queryset(self):
         queryset = AdditionalInfo.objects.all()
